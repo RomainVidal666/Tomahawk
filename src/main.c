71b182a8b9f25979637bded6865b4b71363cd58d
thomasjacumin666 <jacumin.thomas@gmail.com>
2016-04-13 10:27:06 +0000

    Ajout des nouveaux tests.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 #include "api.h"
 
 void callback(char* string, int len) {
-    printf("CALLBACK : Trouve : %s\n", string);
+    printf("CALLBACK : Trouve : (%s)\n", string);
 }
 
 int main ( int argc, char * argv [] ) {
@@ -25,16 +25,16 @@ int main ( int argc, char * argv [] ) {
 	/*ok*///char request [] = "POST /fichier.ext HTTP/1.1\r\nHost: www.site.com\r\nConnection: Close\r\nContent-type: application/x-www-form-urlencoded\r\nContent-Length: 33\r\n\r\nvariable=valeur&variable2=valeur2";
 	/*ok*///char request [] = "HTTP/1.1 200 OK\r\nDate: Thu, 11 Jan 2007 14:00:36 GMT\r\nServer: Apache/2.0.54 (Debian GNU/Linux) DAV/2 SVN/1.1.4\r\nConnection: close\r\nTransfer-Encoding: chunked\r\nContent-Type: text/html; charset=ISO-8859-1\r\n\r\n";
 	/*ok*///char request [] = "GET http://www.commentcamarche.net HTTP/1.0\r\nAccept: text/html\r\nIf-Modified-Since: Saturday, 15-January-2000 14:37:11 GMT\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nAccept: text/html\r\nUser-Agent: Mozilla/4.0 (compatible; MSIE 5.0; Windows 95)\r\n\r\n";
-	char request [] = "GET / HTTP/1.1\r\nUser-Agent: Wget/1.16 (linux-gnu)\r\nAccept: */*\r\nHost: www.google.com\r\nConnection: Keep-Alive\r\n\r\n"
+	/*ok*/char request [] = "GET / HTTP/1.1\r\nUser-Agent: Wget/1.16 (linux-gnu)\r\nAccept: */*\r\nHost: www.google.com\r\nConnection: Keep-Alive\r\n\r\n";
 	if ( parse_HTTP_message ( request, &cursor, http_message ) ) { 
 		printf ( "La requete est valide \n" );
-		print_HTTP_Tree ( request, http_message, 0 );
+		//print_HTTP_Tree ( request, http_message, 0 );
 	}
 		//printf("%c\n",request[cursor]);
 	
-	/*printf ( "***************************** PARSER *****************************\n");
-	int nb = parser (request, strlen(request), "field-name" ,callback);
-*/
+	printf ( "***************************** PARSER *****************************\n");
+	int nb = parser (request, strlen(request), "HTTP-version" ,callback);
+
 	free_HTTP_Tree ( http_message );
 
 	return 0;
